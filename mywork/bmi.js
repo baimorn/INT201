0c75f128307fe24ae953719b27c76ff78a74f905
unknown <baimornnitchakarn@gmail.com>
2021-10-15 00:03:20 +0700

    [update]new

--- mywork/bmi.js
+++ mywork/bmi.js
@@ -1,15 +1,33 @@
-const pound = 0.453592 //กำหนดค้าให้ pound = 0.453592 ใช้ const เพราะกำหนดเป็น final
-const inch = 0.0254 //กำหนดค้าให้ inch = 0.0254 ใช้ const เพราะกำหนดเป็น final 
-
-function getBMI (poundtokg, inchtometer){ //function ชื่อ getBMI รับค่า poundtokg, inchtometer
-    let weight = poundtokg * pound //กำหนดให้ weight คำนวณ poundtokg * pound
-    let height = inchtometer * inch //กำหนดให้ height คำนวณ poundtokg * pound
-    return weight / Math.pow(height,2) //สูตรการหา BMI = kg / m^2
+//สร้าง Class ไว้เพื่อเก็บข้อมูลส่วนตัวทั่วไปของนักศึกษา
+class student{
+    constructor(stdid,fname,lname,){
+        this.stdid = stdid;
+        this.firstname = fname;
+        this.lastname = lname;
+    }
 }
 
-console.log(getBMI(36.5,39)) //16.871807182141648
-console.log(getBMI(42.2,40)) //18.543445036890073
-console.log(getBMI(20,32)) //13.731816526133052
+//สร้าง Class เพื่อเก็บรายละเอียดรายวิชา (เพิ่มการสร้าง Class เข้ามาเพื่อเป็นระเบียบในการเก็บเป็น Array)
+class subject{
+    constructor(courseCode , courseName , credit){
+        this._courseCode = courseCode;
+        this._courseName = courseName;
+        this._credit = credit;
+    }
+}
 
-getBMI(36.5,39);
- 
+
+//subject information
+const subject1 = new subject('GEN101' , 'PHYSICALEDUCATION' ,1);
+const subject2 = new subject('GEN121' , 'LEARNING AND PROBLEM SOLVING' , 3);
+const subject3 = new subject('INT100' , 'LEARNING AND PROBLEM SOLVING' , 3);
+const subject4 = new subject('INT101' , 'PROGRAMMING FUNDAMENTALS' , 3);
+const subject5 = new subject('INT102' , 'WEB TECHOLOGY' , 1);
+const subject7 = new subject('GEN121' , 'LEARNING AND PROBLEM SOLVING' , 3);
+const subject8 = new subject('INT114' , 'DISCRETE MATHEMATICS FOR INFORMATION TECHNOLOGY' ,3);
+
+//student information
+let s1 = new student("63130500036" , "Nitchakarn" , "Pasanee");
+
+console.log(s1);
+
